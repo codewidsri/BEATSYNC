777b55c7d59ec232ac0374de1e28421ab33a555b
sriharish kumar <142038601+codewidsri@users.noreply.github.com>
2025-10-08 10:25:07 +0000

    browser router problem fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
+import { HashRouter, Outlet, Route, Routes } from "react-router-dom"
 import Home from "./Home.jsx"
 import Create from "./Create.jsx"
 import NavBar from "./NavBar.jsx"
 
 function App({ mode, toggle }) {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar mode={mode} toggle={toggle} />
       <Outlet />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/create/:roomid" element={<Create />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
